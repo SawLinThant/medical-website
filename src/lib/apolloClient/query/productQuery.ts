62f0d40a8980cbc096c9f797792d627b90d09b89
Saw Lin Thant <115479032+SawLinThant@users.noreply.github.com>
2024-12-28 23:52:02 +0700

    Feature/product detail (#5) (#6)
    * feature/product-detail-page
    
    * fix/params fix

--- src/lib/apolloClient/query/productQuery.ts
+++ src/lib/apolloClient/query/productQuery.ts
@@ -61,3 +61,65 @@ export const GET_TOP_SAVER_PRODUCTS = gql`
     }
   }
 `;
+
+export const GET_PRODUCT_BY_ID = gql`
+  query getProducts($id: uuid!) {
+    products(where: { id: { _eq: $id } }) {
+      id
+      name
+      price
+      bulk_price
+      quantity
+      description
+      shop {
+        id
+        name
+        logo
+        description
+        address
+        phone
+        remark
+        shop_admin_name
+      }
+      category {
+        id
+        name
+      }
+      images {
+        id
+        image_url
+      }
+    }
+  }
+`;
+
+export const GET_PRODUCT_BY_CATEGORY_ID = gql`
+  query getProducts($category_id: uuid!) {
+    products(where: { category_id: { _eq: $category_id } }) {
+      id
+      name
+      price
+      bulk_price
+      quantity
+      description
+      shop {
+        id
+        name
+        logo
+        description
+        address
+        phone
+        remark
+        shop_admin_name
+      }
+      category {
+        id
+        name
+      }
+      images {
+        id
+        image_url
+      }
+    }
+  }
+`;
---
+++ src/modules/common/product-reel/index.tsx
@@ -0,0 +1,72 @@
+"use client"
+
+import * as React from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { cn } from "@/lib/utils";
+import { ChevronRight } from "lucide-react";
+import { Product } from "@/lib/types/global";
+import { Skeleton } from "@/components/ui/skeleton";
+import ProductCard from "../related-prduct-card";
+
+interface BestSellerProps {
+    products: Product[] | null
+}
+
+export function ProductReel({products}:BestSellerProps) {
+    const [productList,setProductList] = React.useState<Product[] | null>(null)
+    React.useEffect(() => {
+        if(products){
+            setProductList(products)
+        }
+    },[setProductList])
+  return (
+    <Carousel className="w-full h-full flex flex-col gap-4">
+      <div className="w-full min-h-16 flex flex-row items-center justify-between">
+        <div className="flex flex-row gap-4 items-center">
+          <h1 className="font-semibold text-xl">Related products</h1>
+          {/* <span className="text-sm text-secondary_color mt-1 flex flex-row items-center">All Products <ChevronRight size={15}/></span> */}
+        </div>
+        <div className="flex flex-row gap-2 h-full items-center">
+          <div className="">
+            <CarouselPrevious className="bg-gray-300 rounded-md hover:bg-gray-400 transition"></CarouselPrevious>
+          </div>
+          <div className="">
+            <CarouselNext
+              className={cn(
+                " bg-gray-300 rounded-md hover:bg-gray-400 transition",
+                {}
+              )}
+            />
+          </div>
+        </div>
+      </div>
+         {productList ? (
+        <CarouselContent className="w-full h-full">
+          {productList.map((product, index) => (
+            <CarouselItem key={index} className="md:basis-1/3 lg:basis-[20%]">
+              <div className="">
+                <ProductCard product={product} />
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      ) : (
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index} className="md:basis-1/3 lg:basis-[20%]">
+              <div className="p-2">
+                <Skeleton className="bg-secondary_color/35 h-[21.8rem] w-full" />
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      )}
+    </Carousel>
+  );
+}
