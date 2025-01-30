47b02abd3e9392f6e1ddc1f8a4272ad0a01a7aa3
Saw Lin Thant <115479032+SawLinThant@users.noreply.github.com>
2025-01-30 13:58:16 +0700

    add/remove http only and secure

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -4,8 +4,8 @@ export const sessionOptions: SessionOptions = {
   password: "noOneisyourSoul##$04034Saw973854868#dsoesldgl09mm34esgsfg6FFew4", 
   cookieName: "myapp_session",
   cookieOptions: {
-    secure: process.env.NODE_ENV === "production", 
-    httpOnly: false,
+   // secure: process.env.NODE_ENV === "production", 
+   // httpOnly: true,
     sameSite: "lax", 
     maxAge: 14 * 24 * 60 * 60, 
   },
