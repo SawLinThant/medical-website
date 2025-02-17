8485061efa02d0dd1874c5a9bef89bf39029350a
AxraTech <100364951+AxraTech@users.noreply.github.com>
2025-02-17 21:31:08 +0630

    Update serverApolloClient.ts

--- src/lib/apolloClient/serverApolloClient.ts
+++ src/lib/apolloClient/serverApolloClient.ts
@@ -3,22 +3,25 @@ import { getCookie } from 'cookies-next';
 import fetch from 'cross-fetch';
 import { getSessionData } from '../utils';
 
-const getTokenFromSession = async () => {
+const createHttpLink = async () => {
   const sessionData = await getSessionData();
-  return sessionData?.token || null; 
-};
-const token = await getTokenFromSession();
-const serverApolloClient = new ApolloClient({
-  ssrMode: true,
-  link: new HttpLink({
+  const token = sessionData?.token || null;
+
+  return new HttpLink({
     uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT,
     fetch,
     headers: {
       ...(token ? { Authorization: `Bearer ${token}` } : {}),
     },
-  }),
-  cache: new InMemoryCache(), 
-});
-
+  });
+};
+const serverApolloClient = async () => {
+  const link = await createHttpLink();
+  return new ApolloClient({
+    ssrMode: true,
+    link,
+    cache: new InMemoryCache(),
+  });
+};
 
 export default serverApolloClient;
